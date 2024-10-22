206eae6825c86d71b85d2b5df764716f8e4a3ae3
jagtym <jagtymxd@gmail.com>
2024-10-22 20:44:01 +0200

    [#2] Add nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "tea";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages."x86_64-linux" = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        name = "tea";
+        src = ./.;
+
+        nativeBuildInputs = [
+          pkgs.gcc
+          pkgs.clang-tools
+          pkgs.clang
+
+          pkgs.protobuf
+          pkgs.pkg-config
+          pkgs.fuse3
+        ];
+
+        buildPhase = ''
+          make
+        '';
+
+        installPhase = ''
+        '';
+      };
+    };
+  };
+}
